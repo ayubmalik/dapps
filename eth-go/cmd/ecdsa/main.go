a2289d919be3067f1e33b8a0e1d945986bf6e302
Ayub Malik <ayub.malik@gmail.com>
2022-05-01 18:09:45 +0100

    feat: adding ECDSA

--- eth-go/cmd/ecdsa/main.go
+++ eth-go/cmd/ecdsa/main.go
@@ -4,19 +4,42 @@ import (
 	"crypto/ecdsa"
 	"fmt"
 	"github.com/ethereum/go-ethereum/crypto"
-
-	"os"
+	"log"
 )
 
 func main() {
 	fmt.Println("generating keys")
-	for i := 0; i < 500; i++ {
-		privateKey, err := genKey()
-		if err != nil {
-			os.Exit(1)
-		}
-		fmt.Printf("private key: %s\n", crypto.PubkeyToAddress(privateKey.PublicKey))
+	key, err := genKey()
+	if err != nil {
+		log.Fatalln(err)
 	}
+
+	hash, _ := stampedHash("hello everyone, its nice to be here aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa ")
+	sig, _ := sign(hash, key)
+	fmt.Println("hash", hash)
+	fmt.Println("sig", sig)
+	pubKey, err := crypto.SigToPub(hash, sig)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("pubKey1", crypto.PubkeyToAddress(key.PublicKey).String())
+	fmt.Println("pubKey2", crypto.PubkeyToAddress(*pubKey).String())
+}
+
+func sign(hash []byte, key *ecdsa.PrivateKey) ([]byte, error) {
+	sig, err := crypto.Sign(hash, key)
+	if err != nil {
+		return nil, err
+	}
+	return sig, nil
+}
+
+func stampedHash(msg string) ([]byte, error) {
+	hash := crypto.Keccak256Hash([]byte(msg))
+	stamp := []byte("\x19malik.eth signed message:\n32")
+	signed := crypto.Keccak256Hash(stamp, hash.Bytes())
+	return signed.Bytes(), nil
 }
 
 func genKey() (*ecdsa.PrivateKey, error) {
