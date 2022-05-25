45c2798470ce3d975d729bf7e343b43d1c27bb9e
Ayub Malik <ayub.malik@gmail.com>
2022-05-25 19:10:33 +0100

    wip: add bip spike

--- eth-go/cmd/bip39/main.go
+++ eth-go/cmd/bip39/main.go
@@ -1,66 +1,35 @@
 package main
 
 import (
+	"bufio"
+	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"regexp"
-	"time"
-	"unsafe"
+	"strconv"
 )
 
 func main() {
-	g := "264F46DCB388AB1744B5722CC66C94573"
-	fmt.Println(len(g))
-	x := byte(189)
-	start := time.Now()
-	p, q := split(x)
-	took := time.Now().Sub(start)
-	fmt.Printf("x = %d, %b\n", x, x)
-	fmt.Printf("p = %d, %b\n", p, p)
-	fmt.Printf("q = %d, %b\n", q, q)
-	fmt.Printf("took %dns\n", took.Nanoseconds())
-	numbers := generateNumbers(12)
-	fmt.Println("got numbers", numbers)
-	i := 23 + 1<<8
-	fmt.Printf("i = %d (%d)\n", i, unsafe.Sizeof(i))
-	fmt.Printf("i = %b\n", i)
-	e, err := entropy()
+	words := loadWords()
+	ent, err := entropy()
 	if err != nil {
 		os.Exit(1)
 	}
-	fmt.Println("bytes = ", e)
-	fmt.Println("bytes = ", string(e))
+
+	indices := wordIndices(ent)
+	phrase := getPhrase(words, indices)
+	fmt.Println("phrase =", phrase)
 }
 
-func split(x byte) (byte, byte) {
-	n := 4
-	var p, q byte
-	for i := 0; i < n; i++ {
-		p = p | (x&0x80)>>7
-		x = x << 1
-		if i < n-1 {
-			p = p << 1
-		}
+func getPhrase(words []string, indices []int) []string {
+	var selected []string
+	for _, i := range indices {
+		selected = append(selected, words[i])
 	}
-	for i := 0; i < n; i++ {
-		q = q | (x&0x80)>>7
-		x = x << 1
-		if i < n-1 {
-			q = q << 1
-		}
-	}
-	return p, q
-}
-
-func chunk(i int) []uint16 {
-	return nil
-}
-
-func generateNumbers(n int) []int {
-	return nil
+	return selected
 }
 
 func entropy() ([]byte, error) {
@@ -77,11 +46,66 @@ func entropy() ([]byte, error) {
 	}
 
 	reg, _ := regexp.Compile(`[^a-f,\d]+`)
+	cleaned := reg.ReplaceAll(body, nil)
+	return hex.DecodeString(string(cleaned))
+}
 
-	dst := make([]byte, 16)
-	_, err = hex.Decode(dst, reg.ReplaceAll(body, nil))
-	if err != nil {
-		return nil, err
+func hash(data []byte) []byte {
+	h := sha256.New()
+	h.Write(data)
+	return h.Sum(nil)
+}
+
+func wordIndices(entropy []byte) []int {
+	hash := hash(entropy)
+	checksum := bin(hash)[:4] // todo - hardcoded for 128 bits
+	full := bin(entropy) + checksum
+	max := len(full) / 11
+	indices := make([]int, max)
+	for i := 0; i < max; i++ {
+		sub := full[i*11 : i*11+11]
+		n, _ := strconv.ParseInt(sub, 2, 64)
+		indices[i] = int(n)
 	}
-	return dst, nil
+	return indices
+}
+
+func bin(data []byte) string {
+	s := ""
+	for _, b := range data {
+		s += fmt.Sprintf("%08b", b)
+	}
+	return s
+}
+
+func loadWords() []string {
+	f, err := os.Open("/home/ayub/projects/dapps/eth-go/cmd/bip39/bip39-wordlist.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer f.Close()
+	words := make([]string, 0)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		words = append(words, scanner.Text())
+	}
+	return words
+}
+
+// spike
+func split(x byte) (byte, byte) {
+	n := 4
+	var p, q byte
+	for i := 0; i < n; i++ {
+		p = p | (x&0x80)>>7
+		x = x << 1
+		p = p << 1
+	}
+	for i := 0; i < n; i++ {
+		q = q | (x&0x80)>>7
+		x = x << 1
+		q = q << 1
+	}
+	return p >> 1, q >> 1
 }
